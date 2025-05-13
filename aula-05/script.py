f79d5156d8a1baaaa04d8910ce5a1ad8a25bf5b5
DAMIAO JUNIOR OLIVEIRA CALADO <damiao.calado@cs.unicid.edu.br>
2025-05-13 20:59:23 -0300

    add aula-10

--- aula-05/script.py
+++ aula-05/script.py
@@ -76,14 +76,14 @@ while(resposta == "sim" or resposta == "s"):
     print("O valor de R é:", r)
     resposta = input("Deseja continuar ?")
     
-
+'''
 
 fatorial = contador = 1
 while (contador <= 5):
     fatorial = fatorial * contador
     contador = contador + 1
 print ("O fatorial de 5 é: ", fatorial)
-'''
-
+nome = ""
+input(nome)
 
 
